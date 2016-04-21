bb4059c3a98cf77cd59c2a31a65a56d1ee6ed49d
Олег Бочагов < >
2016-04-21 17:56:25 +0300

    voting tally

--- src/core.js
+++ src/core.js
@@ -11,3 +11,12 @@ export function next(state) {
     entries: entries.skip(2)
   });
 }
+
+export function vote(state, entry) {
+  const vote = state.get('vote');
+  const tally = vote.get('tally') || Map();
+  const entryTally = tally.get(entry) || 0;
+  return state.set('vote', vote.merge({
+    tally: tally.set(entry, entryTally + 1)
+  }));
+}
